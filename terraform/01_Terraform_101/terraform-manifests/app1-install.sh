8773627a5d0f0e596945d6cf771bb7b06626f388
Himanshu pandey <42787064+Hpandey243@users.noreply.github.com>
2023-01-17 11:47:25 +0530

    Update app1-install.sh

--- terraform/01_Terraform_101/terraform-manifests/app1-install.sh
+++ terraform/01_Terraform_101/terraform-manifests/app1-install.sh
@@ -5,4 +5,4 @@ sudo systemctl enable httpd
 sudo service httpd start  
 sudo echo '<h1>Welcome to EC2  - APP</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo echo '<!DOCTYPE html> <html> <body> <h1> <p>Terraform Demo</p></h1>  </body></html>' | sudo tee /var/www/html/app1/index.html
