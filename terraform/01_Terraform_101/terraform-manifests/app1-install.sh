dc960a25ab47abe365e1dfeadbd1b8d64c0b4a22
Himanshu pandey <42787064+Hpandey243@users.noreply.github.com>
2023-01-17 21:43:51 +0530

    Update app1-install.sh

--- terraform/01_Terraform_101/terraform-manifests/app1-install.sh
+++ terraform/01_Terraform_101/terraform-manifests/app1-install.sh
@@ -4,5 +4,3 @@ sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start  
 sudo echo '<h1>Welcome to EC2  - APP</h1>' | sudo tee /var/www/html/index.html
-sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body> <h1> <p>Terraform Demo</p></h1>  </body></html>' | sudo tee /var/www/html/app1/index.html
